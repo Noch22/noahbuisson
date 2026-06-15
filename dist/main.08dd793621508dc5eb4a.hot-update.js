16a3c204607eb67ced9cd0ea980c019383f2ed87
Noch22 <noah.fakeadresse@gmail.com>
2026-06-15 19:19:22 +1000

    feat(footer): fixing footer links animation

---
+++ dist/main.08dd793621508dc5eb4a.hot-update.js
@@ -0,0 +1,32 @@
+"use strict";
+self["webpackHotUpdatewp_theme"]("main",{
+
+/***/ "./src/main.scss":
+/*!***********************!*\
+  !*** ./src/main.scss ***!
+  \***********************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+    if(true) {
+      // 1781515011373
+      var cssReload = __webpack_require__(/*! ../node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js */ "./node_modules/mini-css-extract-plugin/dist/hmr/hotModuleReplacement.js")(module.id, {"locals":false});
+      module.hot.dispose(cssReload);
+      module.hot.accept(undefined, cssReload);
+    }
+  
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("c2f6cba2c10e0b206297")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=main.08dd793621508dc5eb4a.hot-update.js.map
